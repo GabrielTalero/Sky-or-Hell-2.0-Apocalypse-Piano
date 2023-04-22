896d7328805e0ebeceaeea17ea016ea6795aa7b4
William Andrés Galindo Zabala <88348949+xGibramx@users.noreply.github.com>
2023-04-21 22:56:08 -0500

    Version ALFA 0.01

---
+++ Sky or Hell 2.0 - Apocalypse Piano/Assets/Scrips C#/PianoTile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoTile : MonoBehaviour
+{
+    public int midiNote;
+
+    /*public void PressKeys(){
+       
+    Renderer renderer = whiteTile.GetComponent<Renderer>();
+    renderer.material.color = Color.black;
+
+    }*/
+
+  
+
+
+}
