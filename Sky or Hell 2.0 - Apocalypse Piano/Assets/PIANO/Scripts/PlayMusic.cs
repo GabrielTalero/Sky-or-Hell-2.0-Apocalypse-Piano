bc3eda29f9dcdea1379e9b6d245e8c35ca96e63f
GabrielTalero <87492247+GabrielTalero@users.noreply.github.com>
2023-05-09 13:28:53 -0500

    Sky or Hell 2.0 - v0.47 (3)
    NO SE GUARDO EL ANTERIOR

--- Sky or Hell 2.0 - Apocalypse Piano/Assets/PIANO/Scripts/PlayMusic.cs
+++ Sky or Hell 2.0 - Apocalypse Piano/Assets/PIANO/Scripts/PlayMusic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayMusic : MonoBehaviour
 {
@@ -10,60 +11,101 @@ public class PlayMusic : MonoBehaviour
     public AudioClip[] voices;
 
     public GameObject[] teclasPiano;
-    
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)) {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
             audioSource.PlayOneShot(voices[0]);
-            //teclasPiano.
+            StartCoroutine(ChangeColorCoroutine(0, Color.grey, 0.5f));
         }
-        else if (Input.GetKeyDown(KeyCode.S)) {
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
             audioSource.PlayOneShot(voices[1]);
+            StartCoroutine(ChangeColorCoroutine(1, Color.grey, 0.5f));
         }
-        else if (Input.GetKeyDown(KeyCode.D)) {
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
             audioSource.PlayOneShot(voices[2]);
+            StartCoroutine(ChangeColorCoroutine(2, Color.grey, 0.5f));
         }
-        else if (Input.GetKeyDown(KeyCode.F)) {
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
             audioSource.PlayOneShot(voices[3]);
+            StartCoroutine(ChangeColorCoroutine(3, Color.grey, 0.5f));
         }
-        else if (Input.GetKeyDown(KeyCode.G)) {
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
             audioSource.PlayOneShot(voices[4]);
+            StartCoroutine(ChangeColorCoroutine(4, Color.grey, 0.5f));
         }
-        else if (Input.GetKeyDown(KeyCode.H)) {
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
             audioSource.PlayOneShot(voices[5]);
+            StartCoroutine(ChangeColorCoroutine(5, Color.grey, 0.5f));
         }
-        else if (Input.GetKeyDown(KeyCode.J)) {
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
             audioSource.PlayOneShot(voices[6]);
+            StartCoroutine(ChangeColorCoroutine(6, Color.grey, 0.5f));
         }
-        else if (Input.GetKeyDown(KeyCode.K)) {
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
             audioSource.PlayOneShot(voices[7]);
+            StartCoroutine(ChangeColorCoroutine(7, Color.grey, 0.5f));
         }
-        else if (Input.GetKeyDown(KeyCode.L)) {
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
             audioSource.PlayOneShot(voices[8]);
+            StartCoroutine(ChangeColorCoroutine(8, Color.grey, 0.5f));
         }
-        else if (Input.GetKeyDown(KeyCode.BackQuote)) {
+        else if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
             audioSource.PlayOneShot(voices[9]);
+            StartCoroutine(ChangeColorCoroutine(9, Color.grey, 0.5f));
         }
-        else if(Input.GetKeyDown(KeyCode.W)) {
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
             audioSource.PlayOneShot(voices[10]);
+            StartCoroutine(ChangeColorCoroutine(10, Color.black, 0.5f));
         }
-        else if(Input.GetKeyDown(KeyCode.E)) {
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
             audioSource.PlayOneShot(voices[11]);
+            StartCoroutine(ChangeColorCoroutine(11, Color.black, 0.5f));
         }
-        else if(Input.GetKeyDown(KeyCode.T)) {
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
             audioSource.PlayOneShot(voices[12]);
+            StartCoroutine(ChangeColorCoroutine(12, Color.black, 0.5f));
         }
-        else if(Input.GetKeyDown(KeyCode.Y)) {
+        else if (Input.GetKeyDown(KeyCode.Y))
+        {
             audioSource.PlayOneShot(voices[13]);
+            StartCoroutine(ChangeColorCoroutine(13, Color.black, 0.5f));
         }
-        else if(Input.GetKeyDown(KeyCode.U)) {
+        else if (Input.GetKeyDown(KeyCode.U))
+        {
             audioSource.PlayOneShot(voices[14]);
+            StartCoroutine(ChangeColorCoroutine(14, Color.black, 0.5f));
         }
-        else if(Input.GetKeyDown(KeyCode.O)) {
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
             audioSource.PlayOneShot(voices[15]);
+            StartCoroutine(ChangeColorCoroutine(15, Color.black, 0.5f));
         }
-        else if(Input.GetKeyDown(KeyCode.P)) {
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
             audioSource.PlayOneShot(voices[16]);
+            StartCoroutine(ChangeColorCoroutine(16, Color.black, 0.5f));
         }
+
+    }
+
+    IEnumerator ChangeColorCoroutine(int index, Color color, float delay)
+    {
+        teclasPiano[index].GetComponent<Image>().color = color;
+        yield return new WaitForSeconds(delay);
+        teclasPiano[index].GetComponent<Image>().color = Color.white; // Cambiar de nuevo al color original
     }
 }
